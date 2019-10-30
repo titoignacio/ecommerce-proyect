0c52071f7df159aa604b474d537d46820b673bb5
titoignacio <h.villalobosm03@gmail.com>
2019-10-29 23:12:51 -0300

    modif/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,100 @@
 Order.destroy_all
 Product.destroy_all
 
-12.times do |i|
+
   Product.create(
     name: Faker::Commerce.product_name,
     price: Faker::Commerce.price.to_i,
-    photo: "http://lorempixel.com/400/200/technics/#{i + 1}/"
+    photo: "https://i.imgur.com/G6i2QiD.jpg"
   )
-end
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/oWN0YLO.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/TlQJx5V.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/aCXsg32.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/1uoZz9j.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/uzLms8w.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/CEIDwbS.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/h16aMZ9.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/D9uAif2.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/bsyODkO.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/kLrQD8o.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/hY1DmEw.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/wmlB7wd.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/Ok6BYLV.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/BTKK8in.jpg"
+  )
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://i.imgur.com/jQfEvEz.jpg"
+  )
 AdminUser.create!(email: 'h.villalobosm03@gmail.com', password: '123123', password_confirmation: '123123') if Rails.env.development?
